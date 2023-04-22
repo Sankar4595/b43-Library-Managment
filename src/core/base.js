141f43c4c7bf34f167fb0076f8770b457c232f4c
Sankar4595 <95sankar@gmail.com>
2023-04-22 14:13:21 +0530

    path add

--- src/core/base.js
+++ src/core/base.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Button, Dropdown, DropdownButton } from "react-bootstrap";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-export default function BaseDefault({title,styles,children,newItem}){
+export default function BaseDefault({title,styles,children}){
     const history = useHistory();
 
     return(
@@ -11,10 +11,9 @@ export default function BaseDefault({title,styles,children,newItem}){
             <div className="navbar">
                 <Button onClick={()=>history.push("/")}>Home</Button>
                 <DropdownButton id="dropdown-basic-button" title="Library">
-                    <Button onClick={()=>newItem.push("/economic")}>Economic</Button>
-                    <Dropdown.Item onClick={()=>newItem.push("/economic")}>Economic</Dropdown.Item>
-                    <Dropdown.Item onClick={()=>newItem.push("/history")}>History</Dropdown.Item>
-                    <Dropdown.Item onClick={()=>newItem.push("/tamil")}>Tamil</Dropdown.Item>
+                    <Dropdown.Item onClick={()=>history.push("/economic")}>Economic</Dropdown.Item>
+                    <Dropdown.Item onClick={()=>history.push("/history")}>History</Dropdown.Item>
+                    <Dropdown.Item onClick={()=>history.push("/tamil")}>Tamil</Dropdown.Item>
                 </DropdownButton>
             </div>
             <div className={styles}>{title}</div>
